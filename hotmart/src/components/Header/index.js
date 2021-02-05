feea5be817ba685dfc1e667cf5bd9d13f133806d
Geice Gomes <sogmesmo@gmail.com>
2021-02-05 01:47:49 -0300

    editando home e inserindo imagens

--- hotmart/src/components/Header/index.js
+++ hotmart/src/components/Header/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Background from '../../assets/imgs/gallaxy.svg';
 
 const Header = () => {
     return (
-        <p>Oi sou a Header</p>
+        <div>    
+          <img src={Background} alt='icon' /> 
+        </div>
     )
 
 }
--- hotmart/src/pages/Home/index.js
+++ hotmart/src/pages/Home/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import Entrevista from '../../assets/imgs/about/entrevista.jpg';
+import Tela from '../../assets/imgs/about/tela.jpg';
+import Diviertete from '../../assets/imgs/about/diviertete.jpg';
+import Pride from '../../assets/imgs/about/pride.jpg';
+import Cofe from '../../assets/imgs/about/cofe.jpg';
+import Niver from '../../assets/imgs/about/niver.jpg';
+
 const Home = () => {
     return (
         <article>
             <div  class="about__container">
-            <div  className="about__title"><h2 class="title">
+            <div  className="about__title">
+                <h2 class="title">
         Existimos para permitir que
         as pessoas vivam de suas 
         paixões!
@@ -27,10 +35,95 @@ const Home = () => {
                     parte dessa história!
                 </p>
             </div>
+            <div class="about__photos">
+            <div class="image-slide aspect-ratio--2.2:1">
+            <div class="image-slide__items" >
+                <div class="image-slide__item">
+                    <div class="card-image-caption">
+                        <div class="card-image-caption__image">
+                            <picture class="aspect-ratio--2.2:1 border-radius--rounded image">
+                                <img src={Entrevista}  alt="Gravação em um de nossos estúdios" class=" lazyloaded" />
+                            </picture>
+                                    </div>
+                                    <div class="card-image-caption__wrapper">
+                                        <div class="card-image-caption__caption">
+                        Gravação em um de nossos estúdios
+                </div>
+                </div>
+                </div>
+                </div>
+                <div class="image-slide__item" >
+                    <div class="card-image-caption">
+                        <div class="card-image-caption__image">
+                            <picture class="aspect-ratio--2.2:1 border-radius--rounded image">
+                                <img src={Diviertete} alt="Divirta-se: um dos nossos mantras" class=" lazyloaded"/>
+                                    </picture>
+                                    </div> 
+                                    <div class="card-image-caption__wrapper">
+                                        <div class="card-image-caption__caption">
+                        Divirta-se: um dos nossos mantras
+                </div>
+                </div>
+                </div>
+                </div>
+                <div class="image-slide__item image-slide__item--show">
+                    <div class="card-image-caption">
+                        <div class="card-image-caption__image">
+                            <picture class="aspect-ratio--2.2:1 border-radius--rounded image">
+                                <img src={Tela}  alt="Hack Week: codar pode ser divertido (e apaixonante)!" class=" lazyloaded"/>
+                                    </picture>
+                                    </div> 
+                                    <div class="card-image-caption__wrapper">
+                                        <div class="card-image-caption__caption">
+                         Hack Week: codar pode ser divertido (e apaixonante)!
+                </div>
+                </div>
+                </div>
+                </div>
+                <div class="image-slide__item">
+                    <div class="card-image-caption">
+                        <div class="card-image-caption__image">
+                            <picture class="aspect-ratio--2.2:1 border-radius--rounded image">
+                                <img src={Pride}  alt="Dia do Orgulho LGBTQIA+: na Hotmart todo mundo tem voz!" class=" lazyloaded"/>
+                                </picture>
+                                </div> <div class="card-image-caption__wrapper" ><div class="card-image-caption__caption" >
+                         Dia do Orgulho LGBTQIA+: na Hotmart todo mundo tem voz!
+                </div>
+                </div>
+                </div>
+                </div>
+                <div class="image-slide__item">
+                    <div class="card-image-caption">
+                        <div class="card-image-caption__image">
+                            <picture class="aspect-ratio--2.2:1 border-radius--rounded image">
+                                <img src={Cofe}  alt="Missão Suporte: buscando talentos para atender nossos clientes" class=" lazyloaded"/>
+                                </picture>
+                                </div> 
+                                <div class="card-image-caption__wrapper">
+                                    <div class="card-image-caption__caption">
+                         Missão Suporte: buscando talentos para atender nossos clientes
+                </div>
+                </div>
+                </div>
+                </div>
+                <div class="image-slide__item">
+                    <div class="card-image-caption" >
+                        <div class="card-image-caption__image">
+                            <picture class="aspect-ratio--2.2:1 border-radius--rounded image">
+                            <img src={Niver} alt="Aniversariantes do Mês: sempre um momento muito especial" class=" lazyloaded"/>
+                            </picture>
+                            </div> <div class="card-image-caption__wrapper" ><div class="card-image-caption__caption">
+                         Aniversariantes do Mês: sempre um momento muito especial
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>
+                </div>   
             </div>
-        </article>
+            </div>
+    </article>
     )
-
 }
 
 export default Home ;
